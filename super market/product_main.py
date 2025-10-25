23f79de4c6201a434d746805c722561ef30e4a88
Messbah.A <Messbah.A@Gmail.com>
2025-10-25 16:55:49 +0330

    messbah debugged

--- super market/product_main.py
+++ super market/product_main.py
@@ -1,49 +1,79 @@
 from tkinter import *
 from product_module import *
+from datetime import date
+
+
+def total():
+    total_price = 0
+    for product in product_list:
+        total_price += product["quantity"] * product["price"]
+    messagebox.showinfo("Success", f"Total : {total_price}")
+
+
+def save():
+    try:
+        name_validator(name.get())
+        brand_validator(brand.get())
+        price_validator(price.get())
+        quantity_validator(quantity.get())
+        expire_date_validator(expire_date.get())
+
+        product = {
+            "id": id.get(),
+            "name": name.get(),
+            "brand": brand.get(),
+            "price": price.get(),
+            "quantity": quantity.get(),
+            "expire_date": expire_date.get()
+        }
+        product_list.append(product)
+
+        messagebox.showinfo("Success", "Product has been saved")
+        id.set(0)
+        name.set("")
+        brand.set("")
+        price.set(0)
+        quantity.set(0)
+        expire_date.set(str(date.today()))
+    except Exception as e:
+        messagebox.showerror("Save Error", f"Error : {e}")
+
 
 window = Tk()
 window.title("Super Market")
-window.geometry("500x400")
+window.geometry("300x420")
 
+# id
+Label(window, text="Id").place(x=25, y=25)
+id = IntVar()
+Entry(window, textvariable=id).place(x=100, y=25)
 
-#name
-Label(window, text="name").place(x = 25, y = 25)
+# name
+Label(window, text="Name").place(x=25, y=65)
 name = StringVar()
-Entry(window, textvariable=name).place(x = 100,y = 25)
+Entry(window, textvariable=name).place(x=100, y=65)
 
-#brand
-Label(window, text="brand").place(x = 25, y = 50)
+# brand
+Label(window, text="Brand").place(x=25, y=105)
 brand = StringVar()
-Entry(window, textvariable=brand).place(x = 100,y = 50)
+Entry(window, textvariable=brand).place(x=100, y=105)
 
-#quantity
-Label(window, text="quantity" ).place(x = 25,y = 75)
-quantity= IntVar()
-Entry(window,textvariable=quantity).place(x = 100,y = 75)
+# quantity
+Label(window, text="Quantity").place(x=25, y=145)
+quantity = IntVar()
+Entry(window, textvariable=quantity).place(x=100, y=145)
 
-#price
-Label(window, text="price").place(x = 25,y = 100)
+# price
+Label(window, text="Price").place(x=25, y=185)
 price = IntVar()
-Entry(window, textvariable=price).place(x =100,y = 100)
+Entry(window, textvariable=price).place(x=100, y=185)
 
-# expier_date
-Label(window,text="expire_date").place(x = 25,y = 125)
+# expire_date
+Label(window, text="Expire Date").place(x=25, y=225)
 expire_date = StringVar()
-Entry(window,textvariable=expire_date).place(x = 100,y = 125)
+Entry(window, textvariable=expire_date).place(x=100, y=225)
 
-Button(window,text="Save", command = lambda:save(name,brand,price,quantity,expire_date)).place(x = 100,y = 150, width = 125)
-Button(window,text="Total",command = lambda:total(quantity,price) ).place(x = 100,y = 175, width = 125)
-
+Button(window, text="Save", command=save).place(x=100, y=300, width=80)
+Button(window, text="Total", command=total).place(x=100, y=350, width=80)
 
 window.mainloop()
-
-
-
-
-
-
-
-
-
-
-
--- super market/product_module.py
+++ super market/product_module.py
@@ -1,61 +1,34 @@
 import re
-from datetime import datetime,date,time
+from datetime import datetime, date, time
 from sys import exec_prefix
 from tkinter import messagebox
-product_list=[]
 
-
-
+product_list = []
 
 
 def name_validator(name):
-    if not re.match("^[a-zA-Z\s]{3,30}$",name):
+    if not re.match("^[a-zA-Z\s]{3,30}$", name):
         raise ValueError("Invalid name")
 
+
 def brand_validator(brand):
-    if not re.match("^[a-zA-Z\s]{3,30}$",brand):
+    if not re.match("^[a-zA-Z\s]{3,30}$", brand):
         raise ValueError("Invalid brand")
+
 
 def price_validator(price):
     if not price > 0:
         raise ValueError("Invalid price")
 
+
 def quantity_validator(quantity):
     if not quantity > 0:
         raise ValueError("Invalid quantity")
 
+
 def expire_date_validator(expire_date):
-    if not datetime.strptime(expire_date,'%Y-%m-%d').date() > date.today():
+    if not datetime.strptime(expire_date, '%Y-%m-%d').date() > date.today():
         raise ValueError("Invalid expire")
-
-def total(quantity,price):
-    sum = quantity.get() * price.get()
-    messagebox.showinfo("Success",f"Total:{sum}")
-
-
-def save(name,brand,price,quantity,expire_date):
-    try:
-        name_validator(name.get())
-        brand_validator(brand.get())
-        price_validator(price.get())
-        quantity_validator(quantity.get())
-        expire_date_validator(expire_date.get())
-
-        product = {"name":name.get(),
-                   "brand":brand.get(),
-                   "price":price.get(),
-                   "quantity":quantity.get(),
-                   "expire_date":expire_date.get()}
-        product_list.append(product)
-
-        messagebox.showinfo("Success","Product has been saved")
-        name.set("")
-        brand.set("")
-        price.set(0)
-        quantity.set(0)
-        expire_date.set(0)
-    except Exception as e:
-        messagebox.showerror("Save Error",f"Error : {e}")
 
 
 
