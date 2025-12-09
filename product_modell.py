5367b732713a0dd478dddc790eb1c5fce4c835c6
Parsa Shahrabadi <ostowar021@gmail.com>
2025-12-10 00:47:42 +0330

    Create class Products

---
+++ product_modell.py
@@ -0,0 +1,10 @@
+class Products:
+
+    def __init__(self, id, name, brand, quantity, expier_date, price, total):
+        self.id = id
+        self.name = name
+        self.brand = brand
+        self.quantity = quantity
+        self.price = price
+        self.expire_date = expier_date
+        self.total = total
